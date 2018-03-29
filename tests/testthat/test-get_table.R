bcec7ea6c5a19c2262e96fe13e108e2162797434
Daniel Falbel <dfalbel@gmail.com>
2018-03-29 17:49:04 -0300

    add testing around number of rows

--- tests/testthat/test-get_table.R
+++ tests/testthat/test-get_table.R
@@ -8,6 +8,7 @@ test_that("get table for mdb files", {
   x <- rccess:::get_table(path_mdb, "tblEmployees")
 
   expect_equal(ncol(x), 16)
+  expect_equal(nrow(x), 15)
   expect_named(x)
   expect_is(x, "data.frame")
 
@@ -18,7 +19,9 @@ test_that("get table for accdb files", {
   x <- rccess:::get_table(path_accdb, "Employees")
 
   expect_equal(ncol(x), 8)
+  expect_equal(nrow(x), 43)
   expect_named(x)
   expect_is(x, "data.frame")
+
 
 })
