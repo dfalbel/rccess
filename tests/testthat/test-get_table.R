d4fd001b79dae97ac6f1513069077ea317fc3bf7
Daniel Falbel <dfalbel@gmail.com>
2018-03-29 11:04:49 -0300

    add more tests

--- tests/testthat/test-get_table.R
+++ tests/testthat/test-get_table.R
@@ -1,16 +1,12 @@
 context("get_table functions")
 
-path <- "../../inst/extdata/AgeRange.mdb"
+path_mdb <- "../../inst/extdata/AgeRange.mdb"
+path_accdb <-"../../inst/extdata/Books2010.accdb"
 
-test_that("get_table_names", {
-
-  expect_equal(
-    rccess:::get_table_names(path),
-    "tblEmployees"
-  )
+test_that("get_table_names errors", {
 
   expect_error(
-    rccess:::get_table_names(paste0(path, "wrongPath"))
+    rccess:::get_table_names(paste0(path_mdb, "wrongPath"))
   )
 
   expect_error(
@@ -19,15 +15,60 @@ test_that("get_table_names", {
 
 })
 
-test_that("get_table_schema", {
+test_that("get_table_names for mdb files", {
+
+  expect_equal(
+    rccess:::get_table_names(path_mdb),
+    "tblEmployees"
+  )
+
+})
+
+test_that("get_table_names for accdb files", {
 
   expect_is(
-    rccess:::get_table_schema(path, "tblEmployees"),
-    "data.frame"
+    rccess:::get_table_names(path_accdb),
+    "character"
+  )
+
+  expect_length(
+    rccess:::get_table_names(path_accdb),
+    10L
+  )
+
+})
+
+test_that("get_table_schema errors", {
+
+  expect_error(
+    rccess:::get_table_schema(path_mdb, "wrongTableName")
   )
 
   expect_error(
-    rccess:::get_table_schema(path, "wrongTableName")
+    rccess:::get_table_schema(path_accdb, "wrongTableName")
+  )
+
+})
+
+test_that("get_table_schema for mdb files", {
+
+  expect_is(
+    rccess:::get_table_schema(path_mdb, "tblEmployees"),
+    "data.frame"
+  )
+
+})
+
+test_that("get_table_schema for accdb files", {
+
+  expect_is(
+    rccess:::get_table_schema(path_accdb, "Employees"),
+    "data.frame"
+  )
+
+  expect_is(
+    rccess:::get_table_schema(path_accdb, "AllSex"),
+    "data.frame"
   )
 
 })
